e11f1f9e5d7b9b6d6f57b6f3f65fb5ecf11b95c8
Fabien F <124900622+9999HP@users.noreply.github.com>
2024-04-19 23:48:43 +0200

    Update ab_testing_project.py
    first draft of the comparison graph

--- ab_testing_project.py
+++ ab_testing_project.py
@@ -11,3 +11,12 @@ print(df.describe())
 
 # get the sum of clicks for each group
 print(df.groupby("group").sum("click"))
+
+# Plotting the df using pyplot to get a clear visual comparison
+plt.figure(figsize=(9, 6))
+ax = sns.countplot(data=df, x="group", hue="click", palette="colorblind")
+plt.title("Clicks distribution in both Experimental and Control groups")
+plt.xlabel("Groups")
+plt.ylabel("Count of clicks")
+plt.legend(title="Click?", labels=["No", "Yes"])
+plt.show()
